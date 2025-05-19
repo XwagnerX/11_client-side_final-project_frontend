730f13132fbb4a308cc1cb7a436de457e043ebaf
XwagnerX <wgaleix@gmail.com>
2025-05-19 09:00:00 +0200

    feat: Inicio del proyecto - Estructura base

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,13 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import React from 'react';
+import BookList from './components/BookList/BookList';
+import './App.css';
 
 function App() {
-  const [apiStatus, setApiStatus] = useState(null)
-  const [books, setBooks] = useState([])
-
-  const fetchBooks = async () => {
-    try {
-      console.log('Iniciando petición GET a:', `${import.meta.env.VITE_API_URL}/Book#`)
-      const response = await fetch(`${import.meta.env.VITE_API_URL}/Book`)
-      const data = await response.json()
-      console.log('Datos recibidos de la API:', {
-        data: data
-      })
-      setBooks(data)
-      setApiStatus('success')
-    } catch (error) {
-      console.error('Error en la petición GET:', {
-        url: `${import.meta.env.VITE_API_URL}/Book`,
-        error: error.message,
-        stack: error.stack
-      })
-      setApiStatus('error')
-    }
-  }
-
-  useEffect(() => {
-    fetchBooks()
-  }, [])
-
   return (
-    <div>
+    <div className="App">
+      <BookList />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
